3126dada8ac133e966883b534329d95399d2d6ba
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-02-19 12:47:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default{
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  },
+  components:{
+    AppHeader, AppMain, AppFooter
+  },
+};
+</script>
+
+<template>
+ <AppHeader></AppHeader>
+ <AppMain></AppMain>
+ <AppFooter></AppFooter>
+</template>
+
+<style scoped>
+
+</style>
