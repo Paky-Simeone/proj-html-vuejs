25e473094239b07b3696670dc38423a2ca29620b
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-02-19 17:56:26 +0100

    Header done

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,7 +7,16 @@ export default{
 <section class="header-section">
     <div class="left">
         <div>
-            <img src="../assets/img/logo-restaurant-2x-200x38.png" alt="">
+            <img src="../assets/img/logo-restaurant-2x-300x58.png" alt="">
+        </div>
+        <div class="jumbo-text-up">
+            <p class="linea"></p>
+            <p>THE BEST TABLE IN TOWN</p>
+        </div>
+        <div class="jumbo-text-down">
+            <h1>FINE DINING EXPIRIENCE</h1>
+            <p>Pellentesqye vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.</p>
+            <button>EXPLORE THE MENU</button>
         </div>
     </div>
     
@@ -30,24 +39,71 @@ export default{
 
 <style lang="scss">
 @use '../styles/general.scss';
+@use '../styles/partials/variables' as *;
 .header-section{
     display: flex;
     .left{
         width: 40%;
-        background-color: black;
-        height: 100vh;
-        img{
-            cursor: pointer;
-        }
+        background-color: $black-color;
+        height: 80vh;
+            img{
+                margin-top: 3rem;
+                margin-left: 6rem;
+                cursor: pointer;
+            }
+            .jumbo-text-up{
+                margin-top: 3rem;
+                margin-left: 6rem;
+                display: flex;
+                align-items: center;
+                    .linea{
+                        border: 1px solid $gray-color;
+                        border-radius: 2px;
+                        background-color: $gray-color;
+                        width: 40px;
+                        height: 3px;
+                        margin-right: 2rem;
+                    }
+                    p{
+                        color: $gray-color;
+                    }
+            }
+            .jumbo-text-down{
+                margin-top: 3rem;
+                margin-left: 10.5rem;
+                h1{
+                    color: $white-color;
+                    font-size: xxx-large;
+                }
+                p{
+                    color: $gray-color;
+                    margin-top: 3rem;
+                    font-size: large;
+                    width: 20rem;
+                }
+                button{
+                    border: 1px solid $gray-color;
+                    color: $gray-color;
+                    background: none;
+                    padding: 15px;
+                    margin-top: 2rem;
+                }
+                button:hover{
+                    cursor: pointer;
+                    color: $black-color;
+                    background-color: $white-color;
+                }
+            }
+            
     }
 }
 .right{
     width: 60%;
-    height: 4rem;
+    height: 80vh;
     img{
         position: absolute;
         width: 60%;
-        height: 100%;
+        height: 80vh;
         object-fit: cover;
     }
     .header-nav{
@@ -58,18 +114,21 @@ export default{
         justify-content: right;
         span{
             margin-right: 50px;
-            color: lightgrey;
+            color: $gray-color;
             cursor: pointer;
                 button{
-                    border: 1px solid white;
+                    border: 1px solid $white-color;
                     border-radius: 2px 2px;
-                    background-color: none;
-                    color: white;
+                    background: none;
+                    color: $gray-color;
                     cursor: pointer;
+                }
+                button:hover{
+                    color: $white-color;
                 }
         }
         span:hover{
-            color: white;
+            color: $white-color;
         }
     }
 }  
